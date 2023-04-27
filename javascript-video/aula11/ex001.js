26ac09877de17c7f38b935bf8d620bca727836f4
Marcelo Cabral <kbral197430@gmail.com>
2023-04-27 15:33:27 -0300

    Criação dos Exercícios da aula 11

---
+++ javascript-video/aula11/ex001.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 50) {
+    console.log(`Parabéns o senhor foi multado`)
+}
+console.log(`Não esqueça use sempre cinto de segurança`)
---
+++ javascript-video/aula11/ex002.js
@@ -0,0 +1,8 @@
+var pais = 'França'
+console.log(`Você mora no país ${pais}`)
+if (pais == 'Brasil') {
+    console.log(`Você é Brasileiro`)
+} 
+else {
+    console.log(`Você é Estrangeiro`)
+}
