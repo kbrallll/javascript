df92ebac3026b80c6163c19a14801f2a2c1a8bb2
Marcelo Cabral <kbral197430@gmail.com>
2023-05-24 10:25:16 -0300

    Atualização aula 13

---
+++ javascript-video/aula13/index.js
@@ -0,0 +1,33 @@
+console.log ('Vai começar...')
+for (var c = 1; c <= 500; c++) {
+    console.log(`Passo ${c}`)
+}
+console.log ('Fim!!!')
+
+/*
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 500)
+// Aqui está uma forma com teste lógico no final!!!
+*/
+/*
+var c = 1
+while ( c <= 500) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+//O MESMO CÓDIGO DE CIMA É USADO PARA O CÓDIGO DE BAIXO
+
+
+/*
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+*/
