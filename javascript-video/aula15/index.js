bfe8963181de6d4fc406ce6929e1d7aa113e174f
Marcelo Cabral <kbral197430@gmail.com>
2023-05-26 07:18:55 -0300

    Criação aula 15

---
+++ javascript-video/aula15/index.js
@@ -0,0 +1,18 @@
+let num = [5, 8, 10, 2, 3]
+num[5] =122
+//Aqui estou adicionando um vetor no elemento 6
+num.push(12)
+//Aqui estou adicionando um vetor para a ultima vaga sempre
+num.length
+//Aqui vai saber o tamanho da variável 
+//vai pegar todos os elementos e colocar em ordem crescente
+num.sort()
+console.log(`Nosso vetor tem ${num.length} tantos elementos`)
+console.log(num[0])
+console.log(num[1])
+console.log(num[2])
+console.log(num[3])
+console.log(num[4])
+console.log(num[5])
+console.log(num[6])
+console.log(`Nosso vetor é ${num}`)
